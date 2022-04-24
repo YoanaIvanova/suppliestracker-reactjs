b702d452303b83ceef3f286a824e14339ef093ea
YoanaIvanova <yoana.ve.ivanova@gmail.com>
2022-04-24 19:00:16 +0300

    Show doughnut charts for collection status

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,23 +1,35 @@
-import { Card } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaRegEye } from "react-icons/fa";
+
+import CollectionStatusChart from "./CollectionStatusChart";
 
 const CollectionCard = (props) => {
   return (
     <Card className="collection-card w-100">
       <Card.Body className="d-flex flex-column">
         <Card.Title className="mb-1">{props.collection.name}</Card.Title>
+
         <Card.Subtitle className="mb-3 text-muted">
           {props.collection.items.length} items
         </Card.Subtitle>
-        <Card.Text>{props.collection.description}</Card.Text>
+
+        <Row className="mb-3 justify-content-center align-items-center">
+          <Col md={7}>
+            <Card.Text>{props.collection.description}</Card.Text>
+          </Col>
+          <Col md={5}>
+            <CollectionStatusChart items={props.collection.items} />
+          </Col>
+        </Row>
+
         <Link
           className="btn btn-secondary view-link mt-auto d-flex align-items-center justify-content-center"
           role="button"
           to="/collection"
         >
           <FaRegEye className="me-1" />
-          View colleciton
+          View details
         </Link>
       </Card.Body>
     </Card>
---
+++ src/components/CollectionStatusChart.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+const CollectionStatusChart = (props) => {
+  const [chartData, setChartData] = useState({ datasets: [] });
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+          boxHeight: 5,
+          boxWidth: 5,
+          padding: 5,
+        },
+      },
+    },
+  };
+
+  ChartJS.register(ArcElement, Tooltip);
+
+  useEffect(() => {
+    const own = props.items.filter(
+      (item) => item.status === "OWN"
+    ).length;
+    const want = props.items.filter(
+      (item) => item.status === "WANT"
+    ).length;
+    const doNotWant = props.items.filter(
+      (item) => item.status === "DO_NOT_WANT"
+    ).length;
+
+    setChartData({
+      labels: ["Own", "Want", "Do Not Want"],
+      datasets: [
+        {
+          label: "Items",
+          data: [own, want, doNotWant],
+          backgroundColor: [
+            "rgba(69, 204, 105, 1)",
+            "rgba(214, 66, 49, 1)",
+            "rgba(49, 120, 214, 1)",
+          ],
+          borderColor: [
+            "rgba(255, 255, 255, 1)",
+            "rgba(255, 255, 255, 1)",
+            "rgba(255, 255, 255, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    });
+  }, [props.items]);
+
+  return (
+    <Doughnut
+      className="status-chart mt-3 mt-lg-0"
+      data={chartData}
+      options={options}
+    />
+  );
+};
+
+export default CollectionStatusChart;
