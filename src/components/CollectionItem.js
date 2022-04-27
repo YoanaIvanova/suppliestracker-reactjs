6d78407f974c4b052a8436efdd9e2e986fa93b2c
YoanaIvanova <yoana.ve.ivanova@gmail.com>
2022-04-27 20:20:06 +0300

    Stylize item cards

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -7,11 +7,16 @@ import {
   BsTriangleFill,
   BsHexagonFill,
   BsNutFill,
+  BsXCircleFill,
+  BsPencilSquare,
+  BsStars,
+  BsEmojiAngry,
+  BsHandThumbsUp,
 } from "react-icons/bs";
 
 const CollectionItem = (props) => {
   const [item, setItem] = useState(null);
-  const shapeMap = new Map([
+  const shapeIconMap = new Map([
     ["CIRCLE", BsCircleFill],
     ["ELLIPSE", BsCircleFill],
     ["CIRCLE_DOT", BsRecordCircle],
@@ -30,17 +35,31 @@ const CollectionItem = (props) => {
       return <></>;
     }
 
-    let TagName = shapeMap.get(item.shape);
-
+    let TagName = shapeIconMap.get(item.shape);
     if (!TagName) {
-      TagName = shapeMap.get("CIRCLE");
+      TagName = shapeIconMap.get("CIRCLE");
     }
+
+    const itemClass = item.status.toLowerCase().replaceAll("_", "-");
 
     return (
       <Col className="d-flex justify-content-center align-items-center">
-        <div className="collection-item w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-          <div className="color-name mb-2 text-center">{item.colorName}</div>
-          <div className="color">
+        <div
+          className={`collection-item ${itemClass} w-100 h-100 p-2 d-flex flex-column justify-content-center align-items-center`}
+        >
+          <div className="header mb-1 w-100 align-self-start d-flex justify-content-between">
+            <div className="start">
+              <BsPencilSquare className="icon" />
+            </div>
+            <div className="end">
+              <BsXCircleFill className="icon" />
+            </div>
+          </div>
+
+          <div className="color-name mt-auto mb-2 text-center">
+            {item.colorName}
+          </div>
+          <div className="color mt-auto">
             <TagName
               size="3.5em"
               color={item.color ? item.color : "var(--bs-gray-300)"}
@@ -56,6 +75,21 @@ const CollectionItem = (props) => {
             />
           </div>
           <div className="color-code">{item.colorCode}</div>
+
+          <div className="footer mt-1 w-100 align-self-start d-flex justify-content-between">
+            <div className="start">
+              {item.status === "OWN" && (
+                <BsHandThumbsUp className="icon status" />
+              )}
+              {item.status === "WANT" && <BsStars className="icon status" />}
+              {item.status === "DO_NOT_WANT" && (
+                <BsEmojiAngry className="icon status" />
+              )}
+            </div>
+            <div className="end">
+              {item.qty > 0 && <span className="text">x{item.qty}</span>}
+            </div>
+          </div>
         </div>
       </Col>
     );
