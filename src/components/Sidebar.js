e95deb960eb0a8c69eb4b0ade02c41de91379551
YoanaIvanova <yoana.ve.ivanova@gmail.com>
2022-05-01 15:53:57 +0300

    Use simplebar-react for a prettier sidebar scrollbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,6 +10,9 @@ import {
 import { AiOutlineCloseSquare } from "react-icons/ai";
 import { ImPencil2 } from "react-icons/im";
 import { BsGrid1X2Fill } from "react-icons/bs";
+import SimpleBar from "simplebar-react";
+
+import "simplebar/dist/simplebar.min.css";
 
 import { CollectionsContext } from "../providers/CollectionsProvider";
 
@@ -49,121 +52,123 @@ const Sidebar = () => {
       <div
         className={`collapse ${
           showMobileNav ? "show" : ""
-        } sidebar h-100 bg-primary px-4 d-lg-block`}
+        } sidebar h-100 bg-primary d-lg-block`}
       >
-        <Nav className="flex-column sidebar-menu">
-          <Navbar className="mt-5 mb-4 p-0 d-flex align-items-center">
-            <Navbar.Brand
-              as={Link}
-              to="/collections"
-              className="justify-content-start p-0"
-            >
-              <GiPencilBrush size="1.5em" className="me-2" />
-              <span className="font-logo">Supplies Tracker</span>
-            </Navbar.Brand>
+        <SimpleBar style={{ maxHeight: "100%" }}>
+          <Nav className="flex-column sidebar-menu px-4">
+            <Navbar className="mt-5 mb-4 p-0 d-flex align-items-center">
+              <Navbar.Brand
+                as={Link}
+                to="/collections"
+                className="justify-content-start p-0"
+              >
+                <GiPencilBrush size="1.5em" className="me-2" />
+                <span className="font-logo">Supplies Tracker</span>
+              </Navbar.Brand>
+
+              <Nav.Link
+                className="collapse-close d-lg-none ms-auto p-0 ms-2"
+                onClick={() => setShowMobileNav(false)}
+              >
+                <AiOutlineCloseSquare size="2em" />
+              </Nav.Link>
+            </Navbar>
 
             <Nav.Link
-              className="collapse-close d-lg-none ms-auto p-0 ms-2"
-              onClick={() => setShowMobileNav(false)}
+              as={NavLink}
+              to="/collections"
+              onClick={() => {
+                if (showMobileNav) {
+                  setShowMobileNav(false);
+                }
+              }}
+              className="d-flex justify-content-between align-items-center"
             >
-              <AiOutlineCloseSquare size="2em" />
+              <span>
+                <BsGrid1X2Fill className="me-2" />
+                Overview
+              </span>
             </Nav.Link>
-          </Navbar>
 
-          <Nav.Link
-            as={NavLink}
-            to="/collections"
-            onClick={() => {
-              if (showMobileNav) {
-                setShowMobileNav(false);
-              }
-            }}
-            className="d-flex justify-content-between align-items-center"
-          >
-            <span>
-              <BsGrid1X2Fill className="me-2" />
-              Overview
-            </span>
-          </Nav.Link>
+            <Accordion
+              as={Nav.Item}
+              activeKey={accordionActiveKey}
+              onSelect={(e) => setAccordionActiveKey(e)}
+            >
+              <Accordion.Item className="bg-primary" eventKey="collection/">
+                <Accordion.Button
+                  as={NavLink}
+                  to="/collection"
+                  onClick={(e) => e.preventDefault()}
+                  className="d-flex justify-content-between align-items-center nav-link"
+                >
+                  <span>
+                    <ImPencil2 className="me-2" />
+                    Collections
+                    <Badge pill className="bg-light ms-2">
+                      {collectionsContext.collections.length}
+                    </Badge>
+                  </span>
+                </Accordion.Button>
 
-          <Accordion
-            as={Nav.Item}
-            activeKey={accordionActiveKey}
-            onSelect={(e) => setAccordionActiveKey(e)}
-          >
-            <Accordion.Item className="bg-primary" eventKey="collection/">
-              <Accordion.Button
-                as={NavLink}
-                to="/collection"
-                onClick={(e) => e.preventDefault()}
-                className="d-flex justify-content-between align-items-center nav-link"
-              >
-                <span>
-                  <ImPencil2 className="me-2" />
-                  Collections
-                  <Badge pill className="bg-light ms-2">
-                    {collectionsContext.collections.length}
-                  </Badge>
-                </span>
-              </Accordion.Button>
+                <Accordion.Body>
+                  {collectionsContext.collections.map((collection, index) => (
+                    <Nav key={index} className="flex-column">
+                      <Nav.Link
+                        as={NavLink}
+                        to={`/collection/${collection.id}`}
+                        onClick={() => {
+                          if (showMobileNav) {
+                            setShowMobileNav(false);
+                          }
+                        }}
+                        className="d-flex justify-content-between"
+                      >
+                        <span>{collection.name}</span>
+                        <span>{collection.items?.length}</span>
+                      </Nav.Link>
+                    </Nav>
+                  ))}
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
 
-              <Accordion.Body>
-                {collectionsContext.collections.map((collection, index) => (
-                  <Nav key={index} className="flex-column">
-                    <Nav.Link
-                      as={NavLink}
-                      to={`/collection/${collection.id}`}
-                      onClick={() => {
-                        if (showMobileNav) {
-                          setShowMobileNav(false);
-                        }
-                      }}
-                      className="d-flex justify-content-between"
-                    >
-                      <span>{collection.name}</span>
-                      <span>{collection.items?.length}</span>
-                    </Nav.Link>
-                  </Nav>
-                ))}
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-
-          <Nav.Link
-            as={NavLink}
-            to="/favorites"
-            onClick={() => {
-              if (showMobileNav) {
-                setShowMobileNav(false);
-              }
-            }}
-            className="d-flex justify-content-between align-items-center"
-          >
-            <span>
-              <GiHearts className="me-2" />
-              Favorites
-              {/* <Badge pill className="bg-light ms-2">
+            <Nav.Link
+              as={NavLink}
+              to="/favorites"
+              onClick={() => {
+                if (showMobileNav) {
+                  setShowMobileNav(false);
+                }
+              }}
+              className="d-flex justify-content-between align-items-center"
+            >
+              <span>
+                <GiHearts className="me-2" />
+                Favorites
+                {/* <Badge pill className="bg-light ms-2">
                 999+
               </Badge> */}
-            </span>
-          </Nav.Link>
+              </span>
+            </Nav.Link>
 
-          <Nav.Link
-            as={NavLink}
-            to="/wishlist"
-            onClick={() => {
-              if (showMobileNav) {
-                setShowMobileNav(false);
-              }
-            }}
-            className="d-flex justify-content-between align-items-center"
-          >
-            <span>
-              <GiRainbowStar size="1.3em" className="me-1" />
-              Wishlist
-            </span>
-          </Nav.Link>
-        </Nav>
+            <Nav.Link
+              as={NavLink}
+              to="/wishlist"
+              onClick={() => {
+                if (showMobileNav) {
+                  setShowMobileNav(false);
+                }
+              }}
+              className="d-flex justify-content-between align-items-center"
+            >
+              <span>
+                <GiRainbowStar size="1.3em" className="me-1" />
+                Wishlist
+              </span>
+            </Nav.Link>
+          </Nav>
+        </SimpleBar>
       </div>
     </>
   );
